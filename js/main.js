7808f3f1156a523f2ddc013d584baa14d76464d4
Peihan Cui <peihanc@uci.edu>
2026-01-18 20:14:06 -0800

    Integrated bootstrap for responsive pages

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const nav = document.querySelector('.top-nav');
-
-menuBtn.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
